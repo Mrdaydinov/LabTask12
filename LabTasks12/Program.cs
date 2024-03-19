bc6e0ed289382b82598221ee7a0305167b36cf4a
Murad <mrdaydinov@gmail.com>
2024-03-19 12:36:59 +0400

    task 2

--- LabTasks12/Program.cs
+++ LabTasks12/Program.cs
@@ -4,20 +4,24 @@
     {
         static void Main(string[] args)
         {
+            // Task 1
 
-            Console.WriteLine("Enter number:");
-        input:
-            bool check = int.TryParse(Console.ReadLine(), out int input);
-            if (!check)
-            {
-                Console.WriteLine("Not number");
-                goto input;
-            }
+            //    Console.WriteLine("Enter number:");
+            //input:
+            //    bool check = int.TryParse(Console.ReadLine(), out int input);
+            //    if (!check)
+            //    {
+            //        Console.WriteLine("Not number");
+            //        goto input;
+            //    }
 
 
-            
-            Console.WriteLine(ReverseNum(input));
 
+            //    Console.WriteLine(ReverseNum(input));
+
+            // Task 2
+
+            Console.WriteLine(IsSimple(Sum(Fibonacci())));
         }
 
         static int ReverseNum(int input)
@@ -54,6 +58,48 @@
             }
 
             return res;
+        }
+
+
+        static int[] Fibonacci()
+        {
+
+            int[] res = new int[20];
+            int num = 0;
+            int temp = 1;
+            int temp2 = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (num > 100)
+                    break;
+                res[i] = num;
+                num += temp;
+                temp = temp2;
+                temp2 = num;
+            }
+
+            return res;
+        }
+
+        static int Sum(int[] nums)
+        {
+            int sum = 0;
+            foreach (int num in nums)
+                sum += num;
+            return sum;
+        }
+
+        static bool IsSimple(int num)
+        {
+            bool result = true;
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i != 0)
+                    result = false;
+            }
+
+            return result;
         }
     }
 }
